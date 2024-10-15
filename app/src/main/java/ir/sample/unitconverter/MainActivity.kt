9e684f455e567eeabda0960ec9394c52bc24aea9
farzaadshojaei <farzaadshojaei@gmail.com>
2024-10-15 23:21:16 +0330

    Modifying Themes

--- app/src/main/java/ir/sample/unitconverter/MainActivity.kt
+++ app/src/main/java/ir/sample/unitconverter/MainActivity.kt
@@ -1,13 +1,16 @@
 package ir.sample.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -15,25 +18,29 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import ir.sample.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             UnitConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Surface (
+                modifier= Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
 
+            }
 
-
-                }
 
             }
         }
     }
 }
+
+
 
 
 @Composable
@@ -43,8 +50,14 @@ fun UnitCoverter(){
         Text("Unit Converter")
         OutlinedTextField(value = "", onValueChange = {})
         Row {
+            val context= LocalContext.current;
+
+    Button(onClick = { Toast.makeText(context,"Thanks for clicking!", Toast.LENGTH_LONG).show() } ){
+        Text("Click Me")
+    }
 
         }
+        Text("Result:")
     }
 }
 
